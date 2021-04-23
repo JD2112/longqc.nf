82b113720a4b5b00d1881530c767befd09725835
kiwiepic <grpiccoli@gmail.com>
2021-04-24 09:56:25 +1200

    remove extra files

--- mixEM/mixem/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Simple Expectation-Maximization fitting of mixtures of probability densities.
-"""
-
-
-from . import distribution
-from .progress import simple_progress
-from .em import em
-from .model import probability
--- mixEM/mixem/distribution/__init__.py
+++
@@ -1,6 +0,0 @@
-from .distribution import Distribution
-from .lognormal import LogNormalDistribution
-from .normal import NormalDistribution, MultivariateNormalDistribution
-from .exponential import ExponentialDistribution
-
-from .geometric import GeometricDistribution
